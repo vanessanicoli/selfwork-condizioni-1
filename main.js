0136352bd18b93d8c68e3d4852fc89acb5d9fbcd
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-01-11 23:44:51 +0100

    Terminato esercizio switch. Terminato Selfwork Condizioni 1

--- main.js
+++ main.js
@@ -12,26 +12,55 @@
 
 // ------- IF / ELSE -------
 
+// let v = 29;     //Ottimo
+
+// if(v < 18 && v > 0){
+//     console.log('Insufficiente');
+// }
+// else if(v >= 18 && v < 21){
+//     console.log('Sufficiente');
+// }
+// else if(v >= 21 && v < 24){
+//     console.log('Buono');
+// }
+// else if(v >= 24 && v < 27){
+//     console.log('Distinto');
+// }
+// else if(v >= 27 && v <= 29){
+//     console.log('Ottimo');
+// }
+// else if(v == 30){
+//     console.log('Eccellente');
+// }
+// else {
+//     console.log('Voto non valido');
+// }
+
+
+// ------- SWITCH -------
+
 let v = 29;     //Ottimo
 
-if(v < 18 && v > 0){
-    console.log('Insufficiente');
-}
-else if(v >= 18 && v < 21){
-    console.log('Sufficiente');
-}
-else if(v >= 21 && v < 24){
-    console.log('Buono');
-}
-else if(v >= 24 && v < 27){
-    console.log('Distinto');
-}
-else if(v >= 27 && v <= 29){
-    console.log('Ottimo');
-}
-else if(v == 30){
-    console.log('Eccellente');
-}
-else {
-    console.log('Voto non valido');
+switch(true){
+    case v < 18 && v > 0:
+        console.log('Insufficiente');
+        break;
+    case v >= 18 && v < 21:
+        console.log('Sufficiente');
+        break;
+    case v >= 21 && v < 24:
+        console.log('Buono');
+        break;
+    case v >= 24 && v < 27:
+        console.log('Distinto');
+        break;
+    case v >= 27 && v <= 29:
+        console.log('Ottimo');
+        break;
+    case v == 30:
+        console.log('Eccellente');
+        break;
+    default:
+        console.log('Voto non valido');
+        break;
 }
